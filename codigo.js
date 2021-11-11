3b8033eaca5087e8cfd7a1d8f352517af8dbcfa0
Anishass <anisha.sujanani@estudiante.uam.es>
2021-11-11 19:55:13 +0100

    hola

---
+++ codigo.js
@@ -0,0 +1,6 @@
+function addItem(){
+    constcantidad=document.getElementById("input").value
+    consttipo=document.getElementById("options").value
+    console.log(cantidad)
+    console.log(tipo)
+}
